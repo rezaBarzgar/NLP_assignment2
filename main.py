ad7267847c1e42cb7741a0fef0d45f69c31a1eb9
rezabarzgar <reza.barzgar99@gmail.com>
2023-02-01 19:11:31 -0500

    adding functions

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-# This is a sample Python script.
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from utils.utils import *
 
+
+def main():
+    # fetching nltk
+    nltk.download('brown')
+    b_news_sents = [[y.lower() for y in x] for x in brown.sents(categories='news')]
+
+    # training models
+    models = []
+    for number in [10]:  # [1, 2, 3, 4, 5]
+        models.append((number, generate_trained_model(number, tokenized_text=b_news_sents)))
+
+    # generate results
+    data = read_birkbeck('./data/APPLING1DAT.643')
+    results = {}
+    for model in models:
+        model_results = []
+        for sentence in data:
+            prediction = predictions(model=model[1], n=number, text=sentence[2])
+            success_at_1, success_at_5, success_at_10 = success_at_k(correct_word=sentence[1],
+                                                                     predictions_list=prediction)
+            if success_at_1 or success_at_5 or success_at_10:
+                print(sentence)
 
 if __name__ == '__main__':
-    with open('./data/APPLING1DAT.643', 'r') as file:
-        lines = file.readlines()
-        for l in lines:
-            print(l.split())
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
--- utils/utils.py
+++ utils/utils.py
@@ -12,14 +12,14 @@ def generate_trained_model(n, tokenized_text):
     return model
 
 
-def save_model(path: str, model):
+def save_model(path: str, model: nltk.lm.MLE):
     file = open(path, "wb")
     dill.dump(model, file)
     file.close()
     print("model saved successfully at " + path)
 
 
-def load_model(path):
+def load_model(path: str):
     file = open(path, 'rb')
     model = dill.load(file)
     file.close()
@@ -27,14 +27,52 @@ def load_model(path):
     return model
 
 
-def predictions(model, n, text):
-    sentence = text.split(' ')
-    sentence = pad_both_ends(sentence, n=n)
+def predictions(model: nltk.lm.MLE, n, text: str):
+    sentence = list(pad_both_ends(text, n=n))
     results = []
-    for item in model.counts[sentence[sentence.index('*') - n:sentence.index('*')]]:
+    start = sentence.index('*') - n if n<sentence.index('*') else 0
+    end = sentence.index('*')
+    for item in model.counts[sentence[start:end]]:
         results.append(item)
 
     return results
+
+
+def read_birkbeck(path: str):
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        data = []
+        for line in lines:
+            if line[0] == '$':
+                continue
+            temp = line.lower().split()
+            data.append((temp[0], temp[1], temp[2:]))  # Tuple consist of Misspelled word, Correct word, sentence
+        return data
+
+
+def success_at_k(correct_word: str, predictions_list: list):
+    success_at_1 = 0
+    success_at_5 = 0
+    success_at_10 = 0
+    if len(predictions_list) == 0:
+        return success_at_1, success_at_5, success_at_10
+    if predictions_list[0] == correct_word:
+        success_at_1 = 1
+        success_at_5 = 1
+        success_at_10 = 1
+        return success_at_1, success_at_5, success_at_10
+    for word in predictions_list[1:5]:
+        if word == correct_word:
+            success_at_5 = 1
+            success_at_10 = 1
+            return success_at_1, success_at_5, success_at_10
+    for word in predictions_list[5:]:
+        if word == correct_word:
+            success_at_10 = 1
+            return success_at_1, success_at_5, success_at_10
+
+    return success_at_1, success_at_5, success_at_10
+
 
 if __name__ == '__main__':
     # nltk.download('brown')
